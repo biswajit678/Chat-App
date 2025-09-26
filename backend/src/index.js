e37644535d4f3b60f5b8f3a829a2b2c0aebda6ca
Biswajit Nayak <biswajit678@gmail.com>
2025-09-27 01:13:12 +0530

    Ready for deployment: backend + frontend build

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,13 +24,16 @@ app.use(cors({
 app.use("/api/auth",authroutes)
 app.use('/api/messages',messageRoutes)
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "../..//dist")))
+if(process.env.NODE_ENV === "production"){
+    const frontendDistPath = path.join(__dirname, "../../frontend/dist");
 
-    app.get("*", (req,res)=>{
-        res.sendFile(path.join(__dirname, "../../frontend" , "dist" , "index.html"))
-    })
+    app.use(express.static(frontendDistPath));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(frontendDistPath, "index.html"));
+    });
 }
+
 
 server.listen(PORT,()=>{
     console.log(`Server is running on port: ${PORT}`);
