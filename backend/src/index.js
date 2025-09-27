7647797cdec14595c4d8408ceef0d4529965ffd6
Biswajit Nayak <biswajit678@gmail.com>
2025-09-27 12:06:47 +0530

    Your commit message here

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,16 +24,13 @@ app.use(cors({
 app.use("/api/auth",authroutes)
 app.use('/api/messages',messageRoutes)
 
-if(process.env.NODE_ENV === "production"){
-    const frontendDistPath = path.join(__dirname, "../../frontend/dist");
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../..//dist")))
 
-    app.use(express.static(frontendDistPath));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(frontendDistPath, "index.html"));
-    });
+    app.get("*", (_,res)=>{
+        res.sendFile(path.join(__dirname, "../../frontend" , "dist" , "index.html"))
+    })
 }
-
 
 server.listen(PORT,()=>{
     console.log(`Server is running on port: ${PORT}`);
